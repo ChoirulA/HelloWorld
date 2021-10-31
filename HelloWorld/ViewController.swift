788eaa44937e70df7cead6b024543c7c7d9b914b
ChoirulA <choirul.andriansyah@ist.id>
2021-10-31 19:29:41 +0700

    first commit reminder feature

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Choirul Andriansyah on 02/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Reminder", message: "You're stronger than you think", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
